f97a8d3f42b487755e963fb7c2b5ef9968ba3e6e
JATSEN-GESTA <jatsen.gesta@gsfe.tupcavite.edu.ph>
2022-11-23 02:28:17 +0800

    cute

--- jade_components/About.jsx
+++ jade_components/About.jsx
@@ -1,11 +1,28 @@
-import styles from "./About.module.css"
+import React from 'react';
+import styles from "./About.module.css";
+
 
 const About = () => {
     return (
-        <div className={styles.label}> 
-            ABOUT
-
+        <div className={styles.background}>
+            <div className={styles.container}>
+                <div>
+                    <p className={styles.title}>ABOUT THE SYSTEM</p>
+                </div>
+                <div>
+                    <p className={styles.content}>The room monitoring system is a newly established website to assist users on monitoring occupied and vacant rooms 
+                    in the U-Building of TUP Cavite.
+                    </p>
+                    <p className={styles.content}>
+                    Our goal is to minimize the workload of the worker that will check the rooms manually.
+                    </p>
+                    <p className={styles.content}>
+                    Keep exploring our website and try it yourself!</p>
+                </div>
+                
+            </div>
         </div>
+        
 
     );
 };
