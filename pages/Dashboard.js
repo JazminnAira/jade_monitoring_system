bdae55bb1a4cc1f0c273b970747dae28dde03a85
Eunis <Eunis@DESKTOP-86JA1HO>
2022-11-24 02:54:49 +0800

    floor three

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,12 +1,12 @@
-// import dashboardnavbar from "../jade_components/dashboardnavbar"
-// import Floor_twoDb from "../jade_components/Floor_twoDb"
+import dashboardnavbar from "../jade_components/dashboardnavbar"
+import Floor_twoDb from "../jade_components/Floor_twoDb"
 import Floor_threeDb from "../jade_components/Floor_threeDb"
 
 const Dashboard = () => {
     return (
       <div>
-        {/* <dashboardnavbar/>
-        <Floor_twoDb/> */}
+        <dashboardnavbar/>
+        <Floor_twoDb/>
         <Floor_threeDb/>
       </div>
     );
